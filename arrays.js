4319d81f5e5017c938567ac8bd749a27cdbfb012
Amy DuPont <amykdupont@gmail.com>
2020-08-14 23:44:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   return chocolateBars
 }
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1, chocolateBars);
+  chocolateBars.slice(chocolateBars);
   return chocolateBars
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
