2952cb4424c42d9cbacbcb7a7bf4905fa3797db7
Amy DuPont <amykdupont@gmail.com>
2020-08-14 23:12:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,15 +4,19 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(){
   return ["foo", ...chocolateBars];
 }
+addElementToBeginningOfArray();
 
 function destructivelyAddElementToBeginningOfArray(){
   return chocolateBars.unshift(1);
 }
+destructivelyAddElementToBeginningOfArray();
 
 function addElementToEndOfArray(){
   return [...chocolateBars, "foo"];
 }
+addElementToEndOfArray();
 
 function destructivelyAddElementToEndOfArray(){
   return chocolateBars.unshift(1);
 }
+destructivelyAddElementToEndOfArray();
